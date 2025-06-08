f2025c1b9f7783bb6fd8bdb1939b68eebeee4afe
Vagner Nerves <vagnernervessantos@gmail.com>
2025-06-08 13:49:13 -0300

    feat: Add Button on screen and ajusted style.

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,10 +2,14 @@ import { View, Image } from 'react-native'
 
 import { styles } from './style'
 
+import { Button } from '@/components/Button'
+
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
+
+      <Button title="Adicionar" />
     </View>
   )
 }
