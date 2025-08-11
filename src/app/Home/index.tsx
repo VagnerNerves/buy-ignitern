097326cc9bfdc75a4fb762c1c27adf402efd2714
Vagner Nerves <vagnernervessantos@gmail.com>
2025-08-11 10:56:26 -0300

    feat: Create getItems to storage and add on set itens.

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {
   View,
   Image,
@@ -12,34 +12,19 @@ import { styles } from './style'
 
 import { FilterStatus } from '@/types/FilterStatus'
 
+import { itemsStorage, type ItemStorage } from '@/storage/itemsStorage'
+
 import { Button } from '@/components/Button'
 import { Input } from '@/components/Input'
 import { Filter } from '@/components/Filter'
 import { Item } from '@/components/Item'
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-// const ITEMS = [
-//   {
-//     id: '1',
-//     status: FilterStatus.DONE,
-//     description: '1 pacote de café'
-//   },
-//   {
-//     id: '2',
-//     status: FilterStatus.PENDING,
-//     description: '3 pacotes de macarrão'
-//   },
-//   {
-//     id: '3',
-//     status: FilterStatus.PENDING,
-//     description: '3 cebolas'
-//   }
-// ]
 
 export function Home() {
   const [filter, setFilter] = useState<FilterStatus>(FilterStatus.PENDING)
   const [description, setDescription] = useState('')
-  const [itens, setItens] = useState<any>([])
+  const [itens, setItens] = useState<ItemStorage[]>([])
 
   function handleAdd() {
     if (!description.trim()) {
@@ -54,6 +39,20 @@ export function Home() {
 
     setItens(prevState => [...prevState, newItem])
   }
+
+  async function getItems() {
+    try {
+      const response = await itemsStorage.get()
+      setItens(response)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Error', 'Não foi possível filtrar os itens')
+    }
+  }
+
+  useEffect(() => {
+    getItems()
+  }, [filter])
 
   return (
     <View style={styles.container}>
