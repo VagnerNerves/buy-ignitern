064997c1d0745b6cee57e6f717f7e8f05e7da524
Vagner Nerves <vagnernervessantos@gmail.com>
2026-02-02 16:32:27 -0300

    feat: Create function clear to clear storage item.

--- src/storage/itemsStorage.ts
+++ src/storage/itemsStorage.ts
@@ -49,9 +49,18 @@ async function remove(id: string) {
   await save(updatedItems)
 }
 
+async function clear() {
+  try {
+    await AsyncStorage.removeItem(ITEMS_STORAGE_KEY)
+  } catch (error) {
+    throw new Error(`ITEMS_CLEAR: ${error}`)
+  }
+}
+
 export const itemsStorage = {
   get,
   getByStatus,
   add,
-  remove
+  remove,
+  clear
 }
